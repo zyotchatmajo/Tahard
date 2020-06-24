6429f5654e141cde806b853ca2cb29a8614f4795
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2020-06-24 01:48:33 -0500

    Upgraded version - Kevin

---
+++ Assets/testSquare.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSquare : MonoBehaviour
+{
+    public string name;
+    
+    public testSquare(string name)
+    {
+        this.name = name;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
