bb46ae6f0a614434d92220fac39b4a78362afe38
Sergio Herrera <sherrera1695@gmail.com>
2020-06-30 01:06:07 -0500

    Menu
    -Add Assets and scripts

---
+++ Assets/Scripts/CursorOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorOver : MonoBehaviour
+{
+    float speed = 1;
+    bool mouseOver = false;
+    public Color start;
+    public Color end;
+    void OnMouseEnter() {
+        mouseOver = true;
+        transform.Rotate(speed, 0, 0);
+    }
+
+    void OnMouseExit() {
+        mouseOver = false;
+        transform.Rotate(0, 0, 0);
+    }
+
+}
---
+++ Assets/Scripts/RotateY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RotateY : MonoBehaviour {
+    float rotateY = .5f;
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(0, rotateY, 0);
+    }
+
+
+}
